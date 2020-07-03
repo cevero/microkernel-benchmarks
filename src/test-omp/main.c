9d7b36ff81416b57ec120850311e6e0255f37065
reinaldoasf <reinaldoasf@gmail.com>
2020-07-03 20:26:56 -0300

    added multicore print test

--- src/test-omp/main.c
+++ src/test-omp/main.c
@@ -1,53 +1,44 @@
-/*
- * MIT License
- *
- * Copyright(c) 2018 Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 #include <nanvix/sys/perf.h>
 #include <nanvix/sys/thread.h>
 #include <nanvix/ulib.h>
 #include <posix/stdint.h>
-//#include <omp.h>
 #include <kbench.h>
 
-/**
- * @name Benchmark Parameters
- */
-/**@{*/
-#define NTHREADS_MIN                1  /**< Minimum Number of Working Threads      */
+
 #define NTHREADS_MAX  (THREAD_MAX - 1) /**< Maximum Number of Working Threads      */
-#define NTHREADS_STEP               1  /**< Increment on Number of Working Threads */
-/**@}*/
+/*============================================================================*
+ * Benchmark Driver                                                           *
+ *============================================================================*/
 
+/**
+ * @brief Fork-Join Benchmark
+ *
+ * @param argc Argument counter.
+ * @param argv Argument variables.
+ */
+void *Hello_rei(void *teste);
 
-static int NTHREADS; /**< Number of Working Threads */
-int __main2(int argc, const char *argv[])
+int __main2()
 {
-	((void) argc);
-	((void) argv);
+    int i,k;
+//    int id_thread[NTHREADS_MAX];
 
-	uprintf("my test %d",NTHREADS);
+    kthread_t tid[NTHREADS_MAX];
 
-	uprintf("my test %d",NTHREADS);
+    for(i=0; i<NTHREADS_MAX;i++) {
+        kthread_create(&tid[i], Hello_rei, ((void*)( (intptr_t) i)));
+    }
+    for(k = 0; k < NTHREADS_MAX; k++){
+        kthread_join(tid[k], NULL);
+    }
 
 	return (0);
+}
+
+void *Hello_rei(void *teste)
+{
+    int id;
+    id = (int)((intptr_t)teste);
+    uprintf("Hello from thread %d\n",id);
+    return NULL;
 }

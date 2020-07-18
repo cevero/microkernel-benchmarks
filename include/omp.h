9e40891f6facc22d139c5e158627db54107dd996
reinaldoasf <reinaldoasf@gmail.com>
2020-07-18 20:26:56 -0300

    included static omp.h file

---
+++ include/omp.h
@@ -0,0 +1,1 @@
+#include "/home/reinaldo/workspace/microkernel-benchmarks/utils/toolchain/riscv32/gcc-9.1.0/build/riscv32-elf/libgomp/omp.h"
--- src/test-omp/main.c
+++ src/test-omp/main.c
@@ -3,7 +3,7 @@
 #include <nanvix/ulib.h>
 #include <posix/stdint.h>
 #include <kbench.h>
-//#include <omp.h>
+#include <omp.h>
 
 
 #define NTHREADS_MAX  (THREAD_MAX - 1) /**< Maximum Number of Working Threads      */
@@ -26,7 +26,7 @@ int __main2()
 
  //   kthread_t tid[NTHREADS_MAX];
 
-//#pragma omp parallel
+#pragma omp parallel
     uprintf("Hello from thread %d\n",1);
 //    for(i=0; i<NTHREADS_MAX;i++) {
 //        kthread_create(&tid[i], Hello_rei, ((void*)( (intptr_t) i)));
